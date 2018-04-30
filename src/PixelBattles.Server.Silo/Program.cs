136931f8d8b0107a194ecc80d942880ce4cc3050
ihnat.klimchuk <ihnat.klimchuk@gmail.com>
2018-05-01 02:12:11 +0300

    Initial commit

---
+++ src/PixelBattles.Server.Silo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PixelBattles.Server.Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
