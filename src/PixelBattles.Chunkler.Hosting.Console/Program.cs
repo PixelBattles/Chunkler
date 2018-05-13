cec5d5a69539f5f0a5140788caf1d8762d5160c8
ihnat.klimchuk <ihnat.klimchuk@gmail.com>
2018-05-13 23:44:13 +0300

    Slight changes

--- src/PixelBattles.Chunkler.Hosting.Console/Program.cs
+++ src/PixelBattles.Chunkler.Hosting.Console/Program.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace PixelBattles.Chunkler.Hosting
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("Press Enter to terminate...");
+                Console.ReadLine();
+
+                await host.StopAsync();
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var builder = new SiloHostBuilder()
+                // Use localhost clustering for a single local silo
+                .UseLocalhostClustering()
+                // Configure ClusterId and ServiceId
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "MyAwesomeService";
+                })
+            // Configure connectivity
+            .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                // Configure logging with any logging framework that supports Microsoft.Extensions.Logging.
+                // In this particular case it logs using the Microsoft.Extensions.Logging.Console package.
+                .ConfigureLogging(logging => logging.AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
         }
     }
 }
