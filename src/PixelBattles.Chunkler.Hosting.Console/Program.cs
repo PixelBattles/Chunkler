6baa37ba1d8ab2c448f1feab885f3c399270ce44
ihnat.klimchuk <ihnat.klimchuk@gmail.com>
2018-05-13 23:30:43 +0300

    Update template

---
+++ src/PixelBattles.Chunkler.Hosting.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PixelBattles.Chunkler.Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
